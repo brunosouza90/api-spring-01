0e71828bf96602d60213d4f2a9c9ddcae4ed0218
Çbruno <bruno.rodrigues.lopes@outlook.com>
2025-06-27 20:15:22 -0300

    config token ans filters

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import ch.qos.logback.core.net.SyslogOutputStream;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,21 @@ public class TokenService {
                     .sign(algoritmo);
         } catch (JWTCreationException exception){
            throw new RuntimeException("Erro ao gerar JWT");
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT invalido ou expirado");
         }
     }
 
